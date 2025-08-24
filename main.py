78d99a3aa53de6e41f50f567fe51279ac29adf5b
Gerardo <agomezc@ucenfotec.ac.cr>
2025-08-23 21:15:50 -0600

    Basics de Python

---
+++ main.py
@@ -0,0 +1,8 @@
+def hiEveryone(name):
+    print('Este script solo se ejecuta de forma directa!')
+    print(f'Hola {name}')
+
+if __name__ == '__main__':
+    print(f'Hi, everyone.')
+    hiEveryone('Gerardo')
+
